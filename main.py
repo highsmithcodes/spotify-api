dc16f2266b37e99ce75b47f0e7d05eb6b655f508
highsmithcodes <ezrahighsmith@gmail.com>
2023-06-14 22:14:37 -0600

    uodate script

--- main.py
+++ main.py
@@ -1,67 +1,36 @@
-from dotenv import load_dotenv
-import os
-import base64
-from requests import post, get
-import json
+import requests
 
-load_dotenv()
-
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-
-def get_token():
-    auth_string = client_id + ':' + client_secret
-    auth_bytes = auth_string.encode('utf-8')
-    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
+def get_solana_price_data():
+    url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        "vs_currency": "usd",
+        "ids": "solana",
+        "interval": "hourly",
+        "range": "24h"
     }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
 
-    # Convert JSON Data
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
+    response = requests.get(url, params=params)
+    data = response.json()
 
+    return data
 
-# Construct the Header
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
+def detect_trading_patterns(price_data):
+    hourly_prices = [entry['current_price'] for entry in price_data]
 
-# Search for Artist
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"q={artist_name}&type=artist&limit=1"
-    
-    query_url = url + "?" + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-    if len(json_result) == 0:
-        print("No artist with this name exists...")
-        return None
-    
-    return json_result[0]
-    
+    # Insert your trading pattern detection logic here
+    # This is just a simple example to detect an upward trend
+    if hourly_prices[-1] > hourly_prices[0]:
+        pattern = "Upward trend"
+    else:
+        pattern = "No recognizable pattern"
 
-def get_songs_by_artist(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
-    return json_result
+    return pattern
 
+if __name__ == "__main__":
+    solana_price_data = get_solana_price_data()
 
-
-token = get_token()
-result = search_for_artist(token, "Joij")
-artist_id = result["id"]
-songs = get_songs_by_artist(token, artist_id)
-
-
-for idx, song in enumerate(songs):
-    print(f"{idx + 1}. {song['name']}")
+    if solana_price_data:
+        trading_pattern = detect_trading_patterns(solana_price_data)
+        print(f"Trading Pattern: {trading_pattern}")
+    else:
+        print("Failed to retrieve Solana price data.")
