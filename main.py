4adcd9251c3feb00ad5dc70e31035ab720fd0772
highsmithcodes <ezrahighsmith@gmail.com>
2023-06-23 11:11:54 -0600

    add heroku url

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import time
@@ -6,7 +6,7 @@ import time
 # Set up Spotify credentials
 client_id = '043908d5e3844f769aecaad844623854'
 client_secret = '6f4b6912c23e404bb1625c28445ef3fb'
-redirect_uri = '/current-song'  # Must match the redirect URI in your Spotify Developer Dashboard
+redirect_uri = 'https://powerful-thicket-30866-4deb00c4126c.herokuapp.com/current-song'  # Update the redirect URI to match your local development setup
 
 # Create the Spotify client
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope='user-read-playback-state'))
@@ -15,7 +15,13 @@ app = Flask(__name__)
 
 @app.route('/current-song')
 def current_song():
-    raw_input = input()
+    code = request.args.get('code')  # Get the authorization code from the request URL parameters
+    
+    # Check if the code parameter is present
+    if code:
+        # Exchange the authorization code for an access token
+        sp.auth_manager.get_access_token(code, as_dict=False)  # Use as_dict=False to avoid the invalid_request error
+        
     # Get the current user's playback information
     current_playback = sp.current_playback()
 
